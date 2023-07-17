fe7fd7cd35c1b9a2797a10154d130d2d00d0a1d5
Niranjan Rath <niranjan.rath@rabobank.nl>
2023-07-17 11:46:54 +0200

    first commit

---
+++ json-schema-to-java/src/main/java/com/github/innovationforge/workshop/JsonSchemaToJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.innovationforge.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSchemaToJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonSchemaToJavaApplication.class, args);
+	}
+
+}
---
+++ sample-module/src/test/java/com/github/innovationforge/workshop/SpringBootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.innovationforge.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
